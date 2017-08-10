5585ad9e2f69df32caa7fd173733b1248a2be31d
lucas <chenx@ruyi.ai>
2017-08-10 17:52:05 +0800

    update

--- www/ruyiai-official/html/ruyiai-official/js/global-util.js
+++ www/ruyiai-official/html/ruyiai-official/js/global-util.js
@@ -4,14 +4,8 @@ var isproductDomain = true;
 if(window.location.href.indexOf("ruyiai-official/test") > -1){
 	static_host = "http://lab.ruyi.ai/ruyiai-official/test";
 	isproductDomain = false;
-}else if(ruyiai_host.indexOf("127.0.0.1") > -1){
-	static_host = "http://127.0.0.1/gitlab/ruyi-website-prod/ruyiai-official";
-	isproductDomain = false;
 }else if(ruyiai_host.indexOf("http://test.ruyi.ai") > -1){
-	static_host = "http://test.ruyi.ai/gitlab/ruyi-website-prod/ruyiai-official";
-	isproductDomain = false;
-}else if(ruyiai_host.indexOf("http://192.168.1.179") > -1){
-	static_host = "http://192.168.1.179/gitlab/ruyi-website-prod/ruyiai-official";
+	static_host = "http://test.ruyi.ai/gitlab/ruyiai-official/www/ruyiai-official/html/ruyiai-official";
 	isproductDomain = false;
 }else if(ruyiai_host.indexOf("lab.ruyi.ai") > -1){
 	static_host = "http://lab.ruyi.ai/ruyiai-official";
@@ -25,6 +19,24 @@ if(ruyiai_host.indexOf("www") > -1){
 	var myhref = window.location.href;
 	window.location.href = myhref.replace("www.","");
 }
+
+//应该这个方法获取请求参数才是正确的
+function GetRequestTwo() {
+	var url = location.href;
+	var theRequest = new Object();
+	var urlIndex = url.indexOf("?");
+	if (urlIndex != -1) {
+		url = url.substr(urlIndex);
+		var str = url.substr(1);
+		strs = str.split("&");
+		for(var i = 0; i < strs.length; i ++) {
+			theRequest[strs[i].split("=")[0]]=decodeURIComponent(strs[i].split("=")[1]);
+		}
+	}
+	return theRequest;
+}
+var RequestTwo = new Object();
+RequestTwo = GetRequestTwo();
 
 //获取请求参数
 function GetRequest() {
