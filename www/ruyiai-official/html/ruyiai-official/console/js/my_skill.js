b66c6975ac75c194186e83a7ee74893867c0bbc5
zhoubc <zhoubc@ruyi.ai>
2017-09-16 11:26:15 +0800

    new new

--- www/ruyiai-official/html/ruyiai-official/console/js/my_skill.js
+++ www/ruyiai-official/html/ruyiai-official/console/js/my_skill.js
@@ -1,4 +1,13 @@
 function mySkillCtrl($rootScope,$scope, $state, $stateParams){
 	$("[data-act=my_skill]").addClass("active").siblings("li").removeClass("active");
+
+	$('body').on('click', '.my_down_web', function() {
+		$.confirm({
+		        "text": '<div class="my_own_down_div"><label class="down_web_label">是否确认下线该技能？</label>' + '<br>' + '<span class="down_web_span">下线技能后需要重新提交技能进行审核哦！</span></div>',
+		        "title": " ",
+		        "ensureFn": function() {
+		        	}
+		})
+	})
 }
 
