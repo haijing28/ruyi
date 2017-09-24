33d39bc6b87d0625af9c3318f50f559ac02c804f
chaos <2609826031@qq.com>
2017-09-24 23:19:45 +0800

    new

--- www/ruyiai-official/html/ruyiai-official/console/js/skill_publish.js
+++ www/ruyiai-official/html/ruyiai-official/console/js/skill_publish.js
@@ -42,7 +42,7 @@ function skillPublishCtrl($rootScope, $scope, $state, $stateParams) {
 
 	$scope.imgSrc = 'https://dn-vbuluo-static.qbox.me/default-robot.svg';
 	$scope.robotName = '';
-	$scope.awakes = ['111', '222', '333'];
+	$scope.awakes = [{value: ''}];
 	$scope.wrongs = [{value: ''}];
 	$scope.robotDesc = '';
 	$scope.skillTypes = [{name: '音频点播', value: ''},{name: '视频点播', value: ''},{name: '家居指令', value: ''},{name: '语音游戏', value: ''},{name: '电商购物', value: ''},{name: '生活服务', value: ''},{name: '美妆时尚', value: ''},{name: '医疗健康', value: ''},{name: '金融服务', value: ''},{name: '新闻资讯', value: ''},{name: '法律顾问', value: ''},{name: '效率工具', value: ''},{name: '体育健身', value: ''},{name: '测试娱乐', value: ''},{name: '其他', value: ''}]
